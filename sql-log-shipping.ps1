1c589c6b650fecc36d5bda725af53482a0bbb25d
Thomas Stringer <tstringer@outlook.com>
2015-04-01 09:48:36 -0700

    Initial commit

---
+++ sql-log-shipping.ps1
@@ -0,0 +1,11 @@
+﻿param (
+    [string]$SqlServerName
+)
+
+function Get-ConnectionString {
+    return "data source = $SqlServerName; initial catalog = master; trusted_connection = true;"
+}
+
+function Get-LogShippingConfiguration {
+    
+}
